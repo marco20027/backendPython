b3d7689c24d09f27b0b7bb229dfe664d015362e9
Marco <marco.campanale@par-tec.it>
2023-04-14 12:24:29 +0200

    fix api get

--- main.py
+++ main.py
@@ -23,13 +23,34 @@ data = datetime.datetime
 print( data)
 
 @app.get("/getUser")
-def get_User():
-    record = collection.find()
-    list_record = list(record)
-    json_data = dumps(list_record)
-    dataInserimento = str(datetime.datetime.now())
-    return {"response": json_data , "dataInserimento": dataInserimento}
-    
+def get_User(name: str = None, cognome : str = None):
+    if name == None and cognome == None:
+        record = collection.find()
+        list_record = list(record)
+        json_data = dumps(list_record)
+        dataInserimento = str(datetime.datetime.now())
+        return {"response": json_data , "dataInserimento": dataInserimento}
+    else:
+        recordName = collection.find({"name": name})
+        recordCognome = collection.find({"cognome": cognome})
+        responseALL = collection.find({"name": name, "cognome": cognome})
+        listRecordName = list(recordName)
+        listRecordCognome = list(recordCognome)
+        listResponseAll = list(responseALL)
+        json_data_name = dumps(listRecordName)
+        json_data_cognome = dumps(listRecordCognome)
+        json_response_all = dumps(listResponseAll)
+        jsonResponseQueryNome =  {"data": json_data_name}
+        jsonResponseQueryCognome = {"dataCognome": json_data_cognome}
+        if json_data_name:
+            return jsonResponseQueryNome
+        elif recordCognome:
+            return jsonResponseQueryCognome
+        else:
+            return json_response_all
+            
+
+
 
 
 @app.post("/addUser")
@@ -40,10 +61,10 @@ def add_user(name: str, cognome: str, dataNascita : datetime.datetime):
         maggiorenne = True
     else:
         maggiorenne = False    
-    queryAdd = {"name":name, "cognome": cognome, "dataNascita": dataNascita, "maggiorenne":maggiorenne,"dataInserimento" : datetime.datetime.now()}
+    queryAdd = {"name":name, "cognome": cognome, "dataNascita": dataNascita,"dataInserimento" : datetime.datetime.now(), "maggiorenne": maggiorenne}
+    print(queryAdd)
     addRecord = collection.insert_one(queryAdd)
     id = str(addRecord.inserted_id)
     dataInserimento = str(datetime.datetime.now())
-    json_compatible_item_data = {"id" : jsonable_encoder(id), "dataInserimetno": dataInserimento , "maggiorenne": maggiorenne}
+    json_compatible_item_data = {"id" : jsonable_encoder(id), "dataInserimento": dataInserimento }
     return JSONResponse(content=json_compatible_item_data) 
-
