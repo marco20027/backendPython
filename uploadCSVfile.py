154fb40b7690b5e32b4d863be893df76cd255bf9
Marco <marco.campanale@par-tec.it>
2023-04-27 17:39:39 +0200

    upup

--- uploadCSVfile.py
+++ uploadCSVfile.py
@@ -15,12 +15,9 @@ jsonFile = csvFile.to_json()
 data = csvFile.to_dict(orient="records")
 db = mongoClient["dbCSV"]
 lista = []
-dataInserimento = str(datetime.today())
+dataInserimento = {"dataInserimento":datetime.today()}
+lista.append(dataInserimento)
 with open("posWIMTres_HO.json") as file:
     file_data = json.load(file)
-    for i in file_data:
-        timestamp = dataInserimento
-        jsonString = json.dumps(timestamp, indent=4)
-    print(jsonString)
-    jsonString = {"timestamp":jsonString}
-insert = db.csv.insert_many(file_data + jsonString)
+insert = db.csv.insert_many(file_data)
+insertDate= db.csv.insert_many(lista)
