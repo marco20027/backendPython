763b71948ceb0778ecaadba87701331c82e5aded
Marco <marco.campanale@par-tec.it>
2023-05-05 17:33:09 +0200

    push

--- main.py
+++ main.py
@@ -423,11 +423,14 @@ def returnJson():
 @app.post("/updateJson")
 def updateJson():
     with open("posWIMTres_HO.json") as file:
-        data = {"page":1,"per_page":6,"total":12,"total_pages":2,"data":[{"id":1,"name":"cerulean","year":2000,"color":"#98B2D1","pantone_value":"15-4020"},{"id":2,"name":"fuchsia rose","year":2001,"color":"#C74375","pantone_value":"17-2031"},{"id":3,"name":"true red","year":2002,"color":"#BF1932","pantone_value":"19-1664"},{"id":4,"name":"aqua sky","year":2003,"color":"#7BC4C4","pantone_value":"14-4811"},{"id":5,"name":"tigerlily","year":2004,"color":"#E2583E","pantone_value":"17-1456"},{"id":6,"name":"blue turquoise","year":2005,"color":"#53B0AE","pantone_value":"15-5217"}],"support":{"url":"https://reqres.in/#support-heading","text":"To keep ReqRes free, contributions towards server costs are appreciated!"}}
+        data = {"key":"test1"}
     mongoClient = pymongo.MongoClient("mongodb://localhost:27017")
     db = mongoClient["dbCSV"]
     col = db["csv"]
     x = col.insert_one(data)
     for i in col.find():
         print(i)
-    return data
+    return {"messagge":"success"}
+@app.get("/getIdJson")
+def returdId():
+    return
