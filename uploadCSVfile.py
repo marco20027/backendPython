2e6c1ddc754c677a3d135c3b46d4353d816ab4a4
Marco <marco.campanale@par-tec.it>
2023-05-18 11:48:10 +0200

    push

--- uploadCSVfile.py
+++ uploadCSVfile.py
@@ -22,19 +22,16 @@ import json
 
 def csv_to_json(csvFilePath, jsonFilePath):
     jsonArray = []
-      
-    #read csv file
     with open(csvFilePath, encoding='utf-8') as csvf: 
         csvReader = csv.DictReader(csvf) 
-
         for row in csvReader: 
             jsonArray.append(row)
-  
+            print(jsonArray)
     with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
         jsonString = json.dumps(jsonArray, indent=4)
         jsonf.write(jsonString)
+        
           
 csvFilePath = r'posWIMTres_HO.csv'
 jsonFilePath = r'data.json'
-print(jsonFilePath)
 csv_to_json(csvFilePath, jsonFilePath)
